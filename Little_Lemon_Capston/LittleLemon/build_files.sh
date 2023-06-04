ea119703b8eb54beb7515c4fcafc73ebf996c2e6
jahidulsec <jahidul.islam.sec@gmail.com>
2023-06-04 23:02:50 +0600

    adding postgre

--- Little_Lemon_Capston/LittleLemon/build_files.sh
+++ Little_Lemon_Capston/LittleLemon/build_files.sh
@@ -2,8 +2,7 @@
 
 # Build the project
 echo "Building the project..."
-python3 -m pip install django
-python3 -m pip install -r requirements.txt
+pip3 install -r requirements.txt
 
 echo "Collect Static..."
 python3 manage.py collectstatic
