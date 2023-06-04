f745088d85e8b11fba6b4e4edfe9c71ffd8f376e
jahidulsec <jahidul.islam.sec@gmail.com>
2023-06-04 15:05:29 +0600

    adding vercel build file

--- Little_Lemon_Capston/LittleLemon/LittleLemon/settings.py
+++ Little_Lemon_Capston/LittleLemon/LittleLemon/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-udmt&jmli5p3drp80)!@%oqte%he4e!f(#eamcl%*h$uevc$i!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
--- Little_Lemon_Capston/LittleLemon/LittleLemonAPI/models.py
+++ Little_Lemon_Capston/LittleLemon/LittleLemonAPI/models.py
@@ -13,6 +13,7 @@ class MenuItem(models.Model):
     title = models.CharField(max_length=255, db_index= True)
     price = models.DecimalField(max_digits=6, decimal_places=2, db_index=True)
     featured = models.BooleanField(db_index=True)
+    imgUrl = models.URLField(db_index=True, default='')
     category = models.ForeignKey(
         Category, on_delete=models.PROTECT
     )
